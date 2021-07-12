e15f0d665374426a0072a4a4aa31d475e79bda6a
SebastianIslas <sebasislasbecerra@gmail.com>
2021-07-12 17:17:22 -0500

    CarritoFuncionando - Adaptar categorias

--- Frontend/assets/dist/js/script_productos.js
+++ Frontend/assets/dist/js/script_productos.js
@@ -7,61 +7,59 @@
 // Use this file to add JavaScript to your project
 
 
-  const getProducts = async (category) => {
-    let result = await fetch('http://localhost:8080/products/'+category)
-    let resultado = await result.json()
-    return resultado;
-  }
-  
-  const showProducts = async (category) => {
-    console.log("entro2")
-    let data = await getProducts(category)
-    data.results.forEach(data => {
-        let div_productos = document.getElementById("productos");
+const getProducts = async (category) => {
+  let result = await fetch('http://localhost:8080/products/'+category)
+  let resultado = await result.json()
+  return resultado;
+}
 
-        let div = document.createElement("div");
-        div.className = "col mb-5";
-        div_productos.appendChild(div);
-    
-    
-        let img = document.createElement("img");
-        img.setAttribute("src",data.thumbnail);
-        img.className = "card-img-top";
-        div.appendChild(img);
-    
-        //Seccion de detalles del producto
-        let div_details = document.createElement("div");
-        div_details.className= "text-center";
-        div.appendChild(div_details);
-    
-        let h5 = document.createElement("h5");
-        h5.className= "fw-bolder";
-        h5.textContent= data.title
-        div_details.appendChild(h5);
-    
-        let span = document.createElement("span");
-        span.textContent= data.price
-        div_details.appendChild(span);
-    
-        //Seccion del boton    
-        let div_actions = document.createElement("div");
-        div_actions.className = "text-center";
-        div.appendChild(div_actions);
-    
-        let a = document.createElement("a");
-        a.className = "btn btn-outline-dark mt-auto";
-        a.setAttribute('href','#');
-        a.textContent = "Agregar al carrito";
-        div_actions.appendChild(a);
-    
-    });
+const showProducts = async (category) => {
+  console.log("entro2")
+  let data = await getProducts(category)
+  const cards = document.getElementById('cards');
+  const templateCard = document.getElementById('template-card').content;
+  const fragment = document.createDocumentFragment();
+
+  data.results.forEach(data => {
+    templateCard.querySelector('div').setAttribute("id",data.id);
+    templateCard.querySelector('img').setAttribute("src",data.thumbnail);
+    templateCard.querySelector('h5').textContent = data.title;
+    templateCard.querySelector('p').textContent = `${data.price}`;
+    templateCard.querySelector('button').setAttribute('onclick','addToCart("'+data.id+'");');
+    const clone = templateCard.cloneNode(true);
+    fragment.appendChild(clone);
+  });
+  cards.appendChild(fragment);
+        
 
 }
 
 
 function renderProducts(category){
-    const urlParams = new URLSearchParams(window.location.search);
-    showProducts(urlParams.get('category'));
+  const urlParams = new URLSearchParams(window.location.search);
+  showProducts(urlParams.get('category'));
 }
 
 renderProducts();
+
+
+function addToCart(id){
+  console.log("entro")
+  let productos = [];
+  if(!localStorage.getItem('productos')){
+  localStorage.setItem('productos',productos);
+  } else{
+  productos = JSON.parse(localStorage.getItem('productos'));
+  }
+
+  item = document.getElementById(id);
+  console.log(item)
+  productos.push({
+    id: id,
+    nombre: item.querySelector('h5').textContent,
+    precio: item.querySelector('p').textContent
+  })
+  console.log(productos);
+  localStorage.setItem('productos',JSON.stringify(productos));
+  alert("Producto añadido al carrito");
+}
